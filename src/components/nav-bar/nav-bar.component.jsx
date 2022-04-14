a578d9633b489293cb1b6895787b8f2abba5568e
Tushar <tm2582001@gmail.com>
2022-04-14 21:13:42 +0530

    added animation for navbar

--- src/components/nav-bar/nav-bar.component.jsx
+++ src/components/nav-bar/nav-bar.component.jsx
@@ -7,20 +7,39 @@ class NavBar extends React.Component{
     super();
     this.state = {
       isOpen: false,
-     }
+      prevHeight: null,
+      height: null
+    }
+    this.navBar = React.createRef();
   }
 
+  componentDidMount(){
+    this.setState({
+      prevHeight:this.navBar.current.scrollHeight
+    },()=>console.log(this.state.prevHeight))
+  }
 
   toggleNavigation = ()=>{
     this.setState((prevState)=>({
       isOpen: !prevState.isOpen
-    }))
+    }),()=>{
+      this.setState({
+        height:this.navBar.current.scrollHeight
+      })
+      // console.log(this.state.height)
+    })
   }
   
+  // componentDidUpdate(){
+
+  // }
 
   render(){
     return(
-      <div className={"nav-bar"}>
+      <div className={`nav-bar ${this.state.isOpen?"navbar-open":"navbar-close"}`}
+      style={{"--prevHeight":`${this.state.prevHeight}px`,"--height":`${this.state.height}px`}}
+      ref={this.navBar}
+      >
         <div className="logo-div">
           <h1 className="logo">Mentor Plus</h1>
           <NavBarIcon onClick={this.toggleNavigation} className={`nav-bar-icon ${this.state.isOpen?"active":""}`} />
