69c2dcb93b492831e43bc9b4e770cd1c474964fc
Tushar <tm2582001@gmail.com>
2022-04-14 21:08:22 +0530

    make navbar responsive

--- src/components/nav-bar/nav-bar.component.jsx
+++ src/components/nav-bar/nav-bar.component.jsx
@@ -1,29 +1,50 @@
 import React from "react";
+import {ReactComponent as NavBarIcon} from '../../assets/images/bars-solid.svg';
 import "./nav-bar.styles.css";
 
-const NavBar = () => (
-  <div className="nav-bar">
-    <div className="logo-div">
-      <h1 className="logo">Mentor Plus</h1>
-    </div>
-    <div className="nav-bar-icon-div">
-      <div className="nav-bar-icon">
-        <h3>Home</h3>
+class NavBar extends React.Component{
+  constructor(){
+    super();
+    this.state = {
+      isOpen: false,
+     }
+  }
+
+
+  toggleNavigation = ()=>{
+    this.setState((prevState)=>({
+      isOpen: !prevState.isOpen
+    }))
+  }
+  
+
+  render(){
+    return(
+      <div className={"nav-bar"}>
+        <div className="logo-div">
+          <h1 className="logo">Mentor Plus</h1>
+          <NavBarIcon onClick={this.toggleNavigation} className={`nav-bar-icon ${this.state.isOpen?"active":""}`} />
+        </div>
+        <div className={`nav-bar-button-div ${this.state.isOpen?"open":""}`}>
+          <div className="nav-bar-button">
+            <h3>Home</h3>
+          </div>
+          <div className="nav-bar-button">
+            <h3>Profile</h3>
+          </div>
+          <div className="nav-bar-button">
+            <h3> </h3>
+          </div>
+          <div className="nav-bar-button">
+            <h3> </h3>
+          </div>
+          <div className="nav-bar-button">
+            <h3> </h3>
+          </div>
+        </div>
       </div>
-      <div className="nav-bar-icon">
-        <h3>Profile</h3>
-      </div>
-      <div className="nav-bar-icon">
-        <h3> </h3>
-      </div>
-      <div className="nav-bar-icon">
-        <h3> </h3>
-      </div>
-      <div className="nav-bar-icon">
-        <h3> </h3>
-      </div>
-    </div>
-  </div>
-);
+    )
+  }
+} 
 
 export default NavBar;
