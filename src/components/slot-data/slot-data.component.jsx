2244ace3e9c1b19cd1efd5abf4d78d2befbfc486
Tushar <tm2582001@gmail.com>
2022-04-15 21:34:53 +0530

    added date-slot and slot-date

--- src/components/slot-data/slot-data.component.jsx
+++ src/components/slot-data/slot-data.component.jsx
@@ -1,10 +1,14 @@
 import React from "react";
-import './slot-data.styles.css';
+import "./slot-data.styles.css";
 
-const SlotData = ()=>(
-    <div>
-        working
-    </div>
+const SlotData = ({ slotData, isActive,handleClick,id }) => (
+  <div onClick={() => handleClick(id)} className={`date-slot ${isActive ? "active-slot" : ""}`}>
+    <p>{`${slotData.hour} ${
+      slotData.min !== 0 ? ": " + slotData.min : ""
+    } PM - ${slotData.hour + 1} ${
+      slotData.min !== 0 ? ": " + slotData.min : ""
+    } PM`}</p>
+  </div>
 );
 
 export default SlotData;
